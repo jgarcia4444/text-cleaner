3050d6df92dd7a22067ad310b331985666a3fb3b
Jake Garcia <bballwiz4@gmail.com>
2026-03-25 15:06:41 -0700

    remove extra spaces works.

--- app/components/ActionButtons.tsx
+++ app/components/ActionButtons.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { PiArrowsInLineHorizontal } from "react-icons/pi";
 
 interface ActionButton {
     label: string,
@@ -10,19 +11,28 @@ export default function ActionButtons({messyText, cleanText}: {messyText: String
 
     const removeExtraSpaces = (): void => {
         let splitMessyText = messyText.split(" ");
-        let rejoinedText = splitMessyText.join("");
+        let filteredText = splitMessyText.filter(ele => ele !== "");
+        let rejoinedText = filteredText.join(" ");
         cleanText(rejoinedText)
     }
 
     const renderButtons = (): ReactNode => {
         const actionButtons: ActionButton[] = [
-            {label: "", icon: "", buttonAction: removeExtraSpaces},
-            {label: "", icon: "", buttonAction: removeExtraSpaces},
-            {label: "", icon: "", buttonAction: removeExtraSpaces},
-            {label: "", icon: "", buttonAction: removeExtraSpaces},
+            {label: "", icon: <PiArrowsInLineHorizontal size={32} color="#00F" />, buttonAction: removeExtraSpaces},
+            {label: "", icon: <PiArrowsInLineHorizontal size={32} color="#00F" />, buttonAction: removeExtraSpaces},
+            {label: "", icon: <PiArrowsInLineHorizontal size={32} color="#00F" />, buttonAction: removeExtraSpaces},
+            {label: "", icon: <PiArrowsInLineHorizontal size={32} color="#00F" />, buttonAction: removeExtraSpaces},
         ]
         return actionButtons.map((actionButtonInfo, i) => {
-
+            const {label, icon, buttonAction} = actionButtonInfo
+            return (
+                <div key={`${label}-${i}`} className="flex flex-col items-center justify-center gap-2">    
+                    <div onClick={buttonAction} className="p-4 rounded shadow bg-blue-400 hover:cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-md hover:opacity-80 hover:bg-blue-300 active:scale-95 active:bg-blue-500">
+                        {icon}
+                    </div>
+                    <small>{label}</small>
+                </div>
+            )
         })
     }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-
 import { useState } from "react";
+
+import ActionButtons from "./components/ActionButtons";
 
 export default function Home() {
   const [messyText, setMessyText] = useState(""); 
@@ -12,14 +13,14 @@ export default function Home() {
 
   return (
     <div className="flex flex-row w-full h-screen items-center justify-between">
-      <div className="w-2/5 h-1/2">
-        <textarea name="input-text" id="input-text" value={messyText} />
+      <div className="w-2/5 h-1/2 flex items-center justify-center px-4">
+        <textarea onChange={(e) => setMessyText(e.target.value)} className="border-2 border-white rounded w-full h-full text-white font-bold" name="input-text" id="input-text" value={messyText} />
       </div>
       <div className="w-1/5">
         <ActionButtons messyText={messyText} cleanText={handleCleanText} />
       </div>
-      <div className="w-2/5">
-        <textarea name="output-text" id="output-text" value={cleanedText} />
+      <div className="w-2/5 h-1/2 flex items-center justify-center px-4">
+        <textarea className="border-2 border-white rounded w-full h-full" name="output-text" id="output-text" value={cleanedText} />
       </div>
     </div>
   );
